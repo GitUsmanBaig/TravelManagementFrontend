77f16688d632a2c90e1c5365f678f65182946d90
huzaifa <mhuzaifaahmad@gmail.com>
2023-12-24 13:26:57 +0500

    Add routing and navbar for Travel Agency Panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+
+import TravelAgencyNavbar from "./TravelAgencyPanel/Components/TravelAgencyNavbar";
+import TravelAgencyHome from "./TravelAgencyPanel/Pages/TravelAgencyHome";
+//import TravelAgencySignup from "./TravelAgencyPanel/Pages/TravelAgencySignup";
+
 function App() {
-  return <>App</>;
+  const TravelAgencyOutlet = () => {
+    return (
+      <>
+        <TravelAgencyNavbar />
+        <Outlet />
+      </>
+    );
+  };
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/travel-agency/" element={<TravelAgencyOutlet />}>
+            <Route index element={<TravelAgencyHome />} />
+            <Route path="register" element={<div>Signup</div>} />
+            <Route path="login" element={<div>Login</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
